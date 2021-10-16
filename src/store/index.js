a62c25a317064197fe4d59ee3f8ba6e020401894
Joaquim Perez <jivkim@gmail.com>
2021-10-16 16:26:48 +0200

    add data that describe glass dose and bottles in stock

--- src/store/index.js
+++ src/store/index.js
@@ -11,19 +11,44 @@ export default new Vuex.Store({
         id: 1,
         type: 'Goutte',
         name: 'Mirabelle',
-        degree: 45
+        degree: 45,
+        dose: 4,
+        bottles: [
+          {
+            volume: 30,
+            remains: 30
+          }
+        ]
       },
       {
         id: 2,
         type: 'Goutte',
         name: 'Abricotine',
-        degree: 56
+        degree: 56,
+        dose: 4,
+        bottles: [
+          {
+            volume: 70,
+            remains: 10
+          },
+          {
+            volume: 70,
+            remains: 70
+          }
+        ]
       },
       {
         id: 3,
         type: 'Liqueur',
-        name: 'Mirabelle',
-        degree: 45
+        name: 'Suze',
+        degree: 45,
+        dose: 4,
+        bottles: [
+          {
+            volume: 100,
+            remains: 100
+          }
+        ]
       },
     ]
   },
