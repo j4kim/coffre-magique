1516e4117780a2d2a3fe0134e43eb192dd13b333
Joaquim Perez <jivkim@gmail.com>
2021-10-16 16:02:08 +0200

    display data in ul lists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home">
     <h1>Le coffre magique</h1>
+    <ul>
+      <li
+        v-for="type in $store.state.types"
+        :key="type.type"
+      >
+        {{ type.type }}
+        <ul>
+          <li
+            v-for="item in type.items"
+            :key="item.name"
+          >
+            {{ item.name }}
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
