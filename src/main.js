913d59897b0b7c5c6e09f0bd84e550a04b8bf58e
Joaquim Perez <jivkim@gmail.com>
2021-10-27 22:06:55 +0200

    use tailwind

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+import "tailwindcss/tailwind.css"
 
 store.dispatch('fetch')
 
