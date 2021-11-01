27ad2b53ed0c13b1cc533fc0f194d142e1fabac7
Joaquim Perez <jivkim@gmail.com>
2021-11-01 22:14:39 +0100

    use vue-bottle as remaining input

--- src/components/Bottle.vue
+++ src/components/Bottle.vue
@@ -2,30 +2,33 @@
   <div>
     <strong v-if="selected">Bouteille</strong>
     <span v-else @click="$store.commit('selectBottle', index)">Bouteille</span>
-    <input
-      type="number"
-      min="0"
-      :max="bottle.volume"
-      v-model="bottle.remains"
-    > cl 
-    /
-    <input
-      type="number"
-      :min="bottle.remains"
-      max="200"
-      v-model="bottle.volume"
-    > cl 
+    <vue-bottle size="100" v-model="ratio"/>
+    <small>
+      {{ Math.round(bottle.remains) }} / {{ bottle.volume }} cl
+    </small>
   </div>
 </template>
 
 <script>
+import VueBottle from 'vue-bottle'
+
 export default {
+  components: { VueBottle },
+
   props: ['bottle', 'index'],
 
   computed: {
     selected () {
       let index = this.$store.state.editable.selectedBottleIndex || 0
       return index == this.index
+    },
+    ratio: {
+      get () {
+        return this.bottle.remains / this.bottle.volume
+      },
+      set (ratio) {
+        this.bottle.remains = this.bottle.volume * ratio
+      }
     }
   }
 }
