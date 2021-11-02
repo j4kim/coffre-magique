12c668cfcc4b959d854c551ee22db1aaa687798e
Joaquim Perez <jivkim@gmail.com>
2021-11-02 23:26:25 +0100

    set is_admin token only if token is right

--- api/setAdmin.php
+++ api/setAdmin.php
@@ -1,5 +1,9 @@
 <?php
 
-setcookie("is_admin", "1", time()+60*60*24*365);
+$token = file_get_contents('./token.txt');
+
+if ($token === @$_GET['token']) {
+  setcookie("is_admin", "1", time()+60*60*24*365);
+}
 
 header("Location: /");
